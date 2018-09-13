dd0051e0b179c42126a1bc7118200cd34f4738cb
ColeDavis213 <coledavi@ut.utm.edu>
2018-09-13 14:21:34 -0500

    Add project files.

---
+++ Factory Method/Suzuki.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Method
+{
+    class Suzuki : Car
+    {
+        
+
+        private string company;
+        private string model;
+        private int speed;
+    }
+}
